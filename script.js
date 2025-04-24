b5f0b80c808ac049482eeb89e954154051c6a7ea
Yoori Choi <yoori336@gmail.com>
2025-04-24 20:43:16 +0900

    fourty

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const searchBtn = document.getElementById("searchBtn");
+//const searchBtn = document.getElementById("searchBtn");
 
 const handleSearch = (e) => {
   e.preventDefault();
@@ -17,3 +17,27 @@ const handleSearch = (e) => {
 };
 
 searchBtn.addEventListener("click", handleSearch);
+
+// Product View
+const viewItemBtn = document.getElementById("viewItem");
+const addToCart = document.getElementById("addToCart");
+const purchase = document.getElementById("purchase");
+
+const viewItem = () => {
+  gtag("event", "view_item", {
+    currency: "KRW",
+    value: 129000,
+    items: [
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("view_item 이벤트 발생");
+};
+
+viewItemBtn.addEventListener("click", viewItem);
