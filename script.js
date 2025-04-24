bb31e9c1d512820822c307230dba76d717f05465
Yoori Choi <yoori336@gmail.com>
2025-04-24 21:19:54 +0900

    fifth

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ searchBtn.addEventListener("click", handleSearch);
 
 // Product View
 const viewItemBtn = document.getElementById("viewItem");
-const addToCart = document.getElementById("addToCart");
-const purchase = document.getElementById("purchase");
+const addToCartBtn = document.getElementById("addToCart");
+const purchaseBtn = document.getElementById("purchase");
 
 const viewItem = () => {
   gtag("event", "view_item", {
@@ -41,3 +41,45 @@ const viewItem = () => {
 };
 
 viewItemBtn.addEventListener("click", viewItem);
+
+const addToCart = () => {
+  gtag("event", "add_to_cart", {
+    currency: "KRW",
+    value: 129000,
+    items: [
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("add_to_cart 이벤트 발생");
+};
+
+addToCartBtn.addEventListener("click", addToCart);
+
+const purchase = () => {
+  gtag("event", "purchase", {
+    transaction_id: "T12345",
+    affilattion: "카페24몰",
+    currency: "KRW",
+    value: 129000,
+    tax: 12900,
+    shipping: 3000,
+    items: [
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("purchase 이벤트 발생");
+};
+
+purchaseBtn.addEventListener("click", purchase);
